49f97a753d16aaa9f9cdac204ce3f338741b46c2
Silvan Flum <flumsil1@students.zhaw.ch>
2020-12-03 22:42:41 +0100

    Update Main.java

--- src/day1/Main.java
+++ src/day1/Main.java
@@ -2,13 +2,9 @@ package day1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Main {
 
