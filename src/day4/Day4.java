c6eb1d0c901fd12ee18a60a2b5a59a2c562f8af3
Silvan Flum <silvanflum@gmail.com>
2020-12-06 17:41:04 +0100

    Update Day4.java

--- src/day4/Day4.java
+++ src/day4/Day4.java
@@ -1,10 +1,21 @@
 package day4;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Day4 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        String path = "src\\day4\\data.txt";
+        String passports;
 
+        // Copy data.txt into a String
+        Path data = Path.of(path);
+        passports = Files.readString(data);
+
+        
 
     }
 
